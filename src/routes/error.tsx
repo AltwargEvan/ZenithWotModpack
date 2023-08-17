1c6e38b497b2f8bd135d7ffbe14dd9b899e86fb8
AltwargEvan <evanaltwarg2021@gmail.com>
2023-08-17 19:42:46 -0400

    Added twmerge and react router dom Added navbar Added error route

--- src/routes/error.tsx
+++ src/routes/error.tsx
@@ -1,4 +1,5 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Button from "../components/Button";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -11,6 +12,10 @@ export default function ErrorPage() {
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
+      <p>If you found this page, please send us a ticket here:</p>
+      <Button>
+        <Link to="/">Back To App</Link>
+      </Button>
       <p>
         <i>{message}</i>
       </p>
