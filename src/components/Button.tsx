533bda503aa397ce7ffe015695aef99a5db3e1ab
AltwargEvan <evanaltwarg2021@gmail.com>
2023-08-17 19:42:25 -0400

    Created styled button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+type Props = {} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+import { twMerge } from "tailwind-merge";
+
+const Button = ({ className, children, ...props }: Props) => {
+  const mergedClassName = twMerge(
+    className,
+    "bg-primary-500 hover:bg-primary-600 hover:cursor-pointer rounded-lg p-2"
+  );
+  return (
+    <button {...props} className={mergedClassName}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
