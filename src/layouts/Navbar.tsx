f57b3dcd647bd0957bd2067827d1552bfbbd3eaa
AltwargEvan <evanaltwarg2021@gmail.com>
2023-08-17 19:44:37 -0400

    Added rounding to right side of nav

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -11,7 +11,7 @@ const NavLink = ({ to, children }: { to: string; children: ReactNode }) => {
 
 const Navbar = () => {
   return (
-    <div className="bg-secondary-300 flex flex-col p-2">
+    <div className="bg-secondary-300 flex flex-col p-2 rounded-r-xl">
       <div className="px-2 py-1">
         <span className="font-cinzel text-3xl font-bold">ZenitH</span>
       </div>
