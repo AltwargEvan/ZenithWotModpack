c810a1f4fd06b413ae3cf51ce95826cd43c273dc
AltwargEvan <evanaltwarg2021@gmail.com>
2023-10-17 03:06:29 -0400

    fix imports

---
+++ packages/ui/index.ts
@@ -0,0 +1,3 @@
+import { Button } from "./components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "./components/ui/avatar";
+export { Button, Avatar, AvatarFallback, AvatarImage };
