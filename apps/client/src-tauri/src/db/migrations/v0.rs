4c8dc138dee58f159457926b32914ec7892cadbf
AltwargEvan <evanaltwarg2021@gmail.com>
2023-10-18 19:27:41 -0400

    fix v0 migration

--- apps/client/src-tauri/src/db/migrations/v0.rs
+++ apps/client/src-tauri/src/db/migrations/v0.rs
@@ -9,24 +9,28 @@ pub fn v0(tx: &Transaction) -> Result<(), rusqlite::Error> {
             game_directory TEXT
         );
 
-        CREATE TABLE mods (
+        CREATE TABLE mods_in_cache (
             id INTEGER NOT NULL UNIQUE,
-            wg_mods_id INTEGER NOT NULL UNIQUE,
             name TEXT NOT NULL,
             mod_version TEXT NOT NULL,
             game_version TEXT NOT NULL,
-            thumbnail_url TEXT NOT NULL,
             PRIMARY KEY(id)
         );
 
-        CREATE TABLE installed_mods (
+        CREATE TABLE install_configs (
+            id INTEGER NOT NULL UNIQUE,
             name TEXT NOT NULL,
-            mod_id INTEGER NOT NULL,
             mods_path TEXT,
             res_path TEXT,
             configs_path TEXT,
+            mod_id INTEGER NOT NULL,
             game_directory TEXT NOT NULL,
             PRIMARY KEY(name, game_directory)
+        );
+
+        CREATE TABLE installed_configs (
+            config_id INTEGER NOT NULL UNIQUE,
+            game_directory TEXT NOT NULL
         );
         ",
     )?;
