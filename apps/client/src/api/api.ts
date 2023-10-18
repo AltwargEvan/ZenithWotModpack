262577e94cd6af0cca30131c3a1dcb7e38a6c027
AltwargEvan <evanaltwarg2021@gmail.com>
2023-10-17 22:48:12 -0400

    readd streamer live checker

--- apps/client/src/api/api.ts
+++ apps/client/src/api/api.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetch } from "@tauri-apps/api/http";
+import { Body, fetch } from "@tauri-apps/api/http";
 import type { GetModsReturnType } from "@zenith/utils/apitypes";
 
 export const useMods = () => {
@@ -13,3 +13,27 @@ export const useMods = () => {
     queryKey: ["mods"],
   });
 };
+
+export async function getIsStreaming(username: String) {
+  const url = "https://gql.twitch.tv/gql";
+  const query = `query {\n  user(login: \"${username}\") {\n    stream {\n      id\n    }\n  }\n}`;
+
+  const headers = { "client-id": "kimne78kx3ncx6brgo4mv6wki5h1ko" };
+  const body = Body.json({ query });
+  const res = await fetch(url, {
+    method: "POST",
+    body,
+    headers,
+  });
+
+  // @ts-ignore
+  if (res.data.data.user.stream) return true;
+  return false;
+}
+
+export function useIsStreaming(username: String) {
+  return useQuery({
+    queryFn: () => getIsStreaming(username),
+    queryKey: ["isStreaming", username],
+  });
+}
